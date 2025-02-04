4fdc7aa503a1b5189b248b0790b0f6ef19614959
Your Name <you@example.com>
2025-02-04 09:08:30 -0500

    fixed typos

--- lab/oops.sh
+++ lab/oops.sh
@@ -1,11 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
+# note this delates all the vms!
 vm=gateway
 
 sudo virsh shutdown $vm
 sudo virsh destroy $vm
-sudo virsh undefine  gateway --nvram  --remove-all-storage
+sudo virsh undefine $vm --nvram --remove-all-storage
 
 vm=jail
 sudo virsh shutdown $vm
 sudo virsh destroy $vm
-sudo virsh undefine  jail --nvram  --remove-all-storage
+sudo virsh undefine $vm --nvram --remove-all-storage
